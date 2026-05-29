b59b5fbb0839292dff0fe18f24a49a88b451db68
shravyaalake <shravya.alake@gmail.com>
2026-05-29 11:32:28 +0530

    Point frontend to deployed backend API

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -1,5 +1,11 @@
 import axios from "axios";
 
-export const apiClient = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+// export const apiClient = axios.create({
+//   baseURL: "http://127.0.0.1:8000/api",
+// });
+
+const apiClient = axios.create({
+  baseURL: "https://breathe-esg-assignment-g4i4.onrender.com/api",
 });
+
+export default apiClient;
